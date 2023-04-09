791476d8f35c52dae852c4e55d3b35538662689a
Pieter Vardi <pietervardi@gmail.com>
2023-04-09 17:34:54 +0700

    integrasi account dengan backend

--- frontend/src/pages/account/App.tsx
+++ frontend/src/pages/account/App.tsx
@@ -1,85 +1,87 @@
-import React, { useState, useEffect } from 'react';
+import { useContext, useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 import RoomInfo from './RoomInfo';
 import backgroundImage from './images/background.jpg';
-import profilePicture from './images/profile.png';
 import './account.css';
 
-interface RoomInfo {
-    id: number;
-    name: string;
-}
+import { BackButton } from '../../components/Buttons';
+import { ChatroomInfo } from "../../helpers/classes";
+import { API } from "../../helpers/constants";
+import { CurrentUserContext } from '../../context';
+import { useInformativeFetch } from "../../helpers/fetch";
+import { Loading } from '../../components/Informative';
+import { DangerButton } from "../../components/Buttons";
 
-interface UserInfo {
-    name: string;
-    email: string;
-    bio: string;
-    rooms: RoomInfo[];
-}
-
-const user: UserInfo = {
-    name: 'Davin Tristan Lesson',
-    email: 'email@email.com',
-    bio: 'Student of Mikroskil University Medan Computer Science',
-    rooms: [
-        { id: 1, name: 'Room 1' },
-        { id: 2, name: 'Room 2' },
-        { id: 3, name: 'Room 3' },
-        { id: 4, name: 'Room 4' },
-    ],
-};
-
-const App: React.FC = () => {
-    // const [user, setUser] = useState<UserInfo | null>(null);
-
-    // useEffect(() => {
-    //     fetch('/api/user')
-    //         .then((response) => response.json())
-    //         .then((data) => setUser(data));
-    // }, []);
-
-    // if (!user) {
-    //     return <div>Loading...</div>;
-    // }
-
+function App() {
+    const user = useContext(CurrentUserContext);
+    const [myChatrooms, setMyChatrooms] = useState<ChatroomInfo[]>([]);
     const [roomColors, setRoomColors] = useState<string[]>([]);
+    const infoFetch = useInformativeFetch();
+    const navigate = useNavigate();
 
     useEffect(() => {
-        const colors = user.rooms.map(() => `#${Math.floor(Math.random() * 16777215).toString(16)}`);
-        setRoomColors(colors);
+        const fetchChatrooms = async () => {
+            try {
+                const response = await infoFetch(() => fetch(API + "/chatroom/mine", { credentials: "include" }));
+                const json = await response.json();
+                const colors = json.map(() => `#${Math.floor(Math.random() * 16777215).toString(16)}`);
+                setRoomColors(colors);
+                setMyChatrooms(ChatroomInfo.fromJSONArray(json));
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchChatrooms();
     }, []);
+
+    async function deleteAccount() {
+        if (!confirm("Are you sure you want to delete this account?")) return;
+        try {
+            const res = await infoFetch(() => fetch(API + '/accounts', {
+                method: "DELETE",
+                credentials: "include"
+            }));
+            if (res.ok) {
+                navigate("/login", { replace: true });
+            }
+        } catch (e) { };
+    }
 
     return (
         <>
-            <div className="backImage">
-                <img src={backgroundImage} alt="Background" />
-            </div>
-            <div className="profileImage">
-                <div className="pic">
-                    <img src={profilePicture} alt="Profile" />
+            <BackButton />
+            <Loading dependency={user}>
+                <div className="backImage">
+                    <img src={backgroundImage} alt="Background" />
                 </div>
-            </div>
-            <div className="description">
-                <div className="useProfile">
-                    <h1>{user.name}</h1>
-                    <h3>{user.email}</h3>
-                    <p>{user.bio}</p>
-                </div>
-                <div className="info">
-                    <div className="title">
-                        <h3>Ruangan Saya</h3>
-                    </div>
-                    <div className="roomItem">
-                        {user.rooms.map((room, index) => (
-                            <RoomInfo key={room.id} name={room.name} color={roomColors[index]} />
-                        ))}
+                <div className="profileImage">
+                    <div className="pic">
+                        <img src={user?.user?.pfp} alt="Profile" />
                     </div>
                 </div>
-            </div>
-            <div className="button">
-                <button>Delete Account</button>
-            </div>
+                <div className="description">
+                    <div className="useProfile">
+                        <h1>{user?.user?.name}</h1>
+                        <h3>{user?.user?.email}</h3>
+                        <p>{user?.user?.bio}</p>
+                    </div>
+                    <div className="info">
+                        <div className="title">
+                            <h3>Ruangan Saya</h3>
+                        </div>
+                        <div className="roomItem">
+                            {myChatrooms.map((room, index) => (
+                                <RoomInfo key={room.id} name={room.settings.title} color={roomColors[index]} />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <DangerButton onClick={deleteAccount} className="w-25">
+                    Delete This Account
+                </DangerButton>
+            </Loading>
         </>
-    );
+    )
 };
 
 export default App;
